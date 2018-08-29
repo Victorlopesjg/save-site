2e5e19da7aa7e40df357d558fa194e16dfa488b8
Victor Oliveira <victorlopesjg@gmail.com>
2018-08-28 21:22:32 -0300

    Add project

---
+++ src/main/java/victor/ufrn/br/savesite/SavesiteApplication.java
@@ -0,0 +1,12 @@
+package victor.ufrn.br.savesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavesiteApplication.class, args);
+	}
+}
---
+++ src/test/java/victor/ufrn/br/savesite/SavesiteApplicationTests.java
@@ -0,0 +1,16 @@
+package victor.ufrn.br.savesite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SavesiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
