3ef5d56171929768907efb0dd2dd1a01390c1e24
Victor Oliveira <victorlopesjg@gmail.com>
2018-08-29 16:01:40 -0300

    Mudando o properties

--- src/main/java/victor/ufrn/br/savesite/SavesiteApplication.java
+++ src/main/java/victor/ufrn/br/savesite/SavesiteApplication.java
@@ -2,22 +2,25 @@ package victor.ufrn.br.savesite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@ComponentScan
+@EntityScan(basePackages = {"victor.ufrn.br.savesite.model"})
+@EnableJpaRepositories(basePackages = {"victor.ufrn.br.savesite.repository"})
 @EnableSwagger2
 public class SavesiteApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SavesiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SavesiteApplication.class, args);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SavesiteApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SavesiteApplication.class);
+    }
 }
