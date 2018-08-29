e7114e1ebafc178f0fa6a3b3e17c453a93b66e0c
Victor Oliveira <victorlopesjg@gmail.com>
2018-08-28 23:46:55 -0300

    Método de POST

--- src/main/java/victor/ufrn/br/savesite/resource/SiteResource.java
+++ src/main/java/victor/ufrn/br/savesite/resource/SiteResource.java
@@ -1,27 +1,32 @@
 package victor.ufrn.br.savesite.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import victor.ufrn.br.savesite.model.Site;
 import victor.ufrn.br.savesite.repository.SiteRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/site")
 public class SiteResource {
 
     @Autowired
     private SiteRepository siteRepository;
 
-    @RequestMapping("/sites")
-    @GetMapping(produces={"application/json"})
-    public List<Site> listarSites(@RequestParam(required = false, defaultValue = "nome") String sort){
-        return siteRepository.findAll(new Sort(Direction.ASC,sort));
+    @GetMapping(produces = {"application/json"})
+    public List<Site> listarSites(@RequestParam(required = false, defaultValue = "nome") String sort) {
+        return siteRepository.findAll(new Sort(Direction.ASC, sort));
+    }
+
+    @PostMapping(consumes = "application/json", produces = {"application/json"})
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Site inserir(@Valid @RequestBody Site site) {
+        return siteRepository.save(site);
     }
 
 }
