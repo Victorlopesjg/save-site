e91ea0bed05286032bc920adbb39bb1a00050883
Victor Oliveira <victorlopesjg@gmail.com>
2018-08-28 22:04:31 -0300

    TESTE SITE

--- src/main/java/victor/ufrn/br/savesite/SavesiteApplication.java
+++ src/main/java/victor/ufrn/br/savesite/SavesiteApplication.java
@@ -2,11 +2,17 @@ package victor.ufrn.br.savesite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SavesiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SavesiteApplication.class, args);
+	}
+
+	@RequestMapping("/index")
+	public void getAllContatos() {
+		System.out.print("teste");
 	}
 }
