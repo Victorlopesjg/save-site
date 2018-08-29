333d3ddcb95ad84543720ea6a1430d72b45d8919
Victor Oliveira <victorlopesjg@gmail.com>
2018-08-29 00:18:01 -0300

    Configurando Swagger

--- src/main/java/victor/ufrn/br/savesite/SwaggerConfig.java
+++ src/main/java/victor/ufrn/br/savesite/SwaggerConfig.java
@@ -1,19 +1,32 @@
 package victor.ufrn.br.savesite;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.time.LocalDate;
+
 public class SwaggerConfig {
 
+    //    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("victor.ufrn.br.savesite"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
     @Bean
-    public Docket api() {
+    public Docket mainConfig() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("victor.ufrn.br.savesite"))
+                .select().apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .pathMapping("/swagger")
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class);
     }
 }
--- src/main/java/victor/ufrn/br/savesite/resource/SiteResource.java
+++ src/main/java/victor/ufrn/br/savesite/resource/SiteResource.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/site")
+@CrossOrigin
 public class SiteResource {
 
     @Autowired
